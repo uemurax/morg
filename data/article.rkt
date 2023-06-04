b382dcde0f3661d5f72b18ce9510073815d32a21
Taichi Uemura <t.uemura00@gmail.com>
2023-06-05 00:01:37 +0200

    define article

---
+++ data/article.rkt
@@ -0,0 +1,23 @@
+#lang typed/racket
+
+(require "inline.rkt"
+         "block.rkt"
+         "id.rkt")
+
+(provide (struct-out article) Article
+         (struct-out proof) Proof)
+
+(struct article
+  ([id : Id]
+   [header : (Option Inline)]
+   [title : (Option Inline)]
+   [contents : Block]
+   [proof : (Option Proof)])
+  #:transparent
+  #:type-name Article)
+
+(struct proof
+  ([header : Inline]
+   [contents : Block])
+  #:transparent
+  #:type-name Proof)
