e495568d89a70451889197d60186217839513ae7
Taichi Uemura <t.uemura00@gmail.com>
2023-06-06 15:15:29 +0200

    rename

--- info.rkt
+++ info.rkt
@@ -1,8 +1,8 @@
 #lang info
-(define collection "morg")
+
+(define collection 'multi)
 (define deps '("base"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
-(define scribblings '(("scribblings/morg.scrbl" ())))
 (define pkg-desc "Organize your mathematical thoughts.")
 (define version "0.0")
 (define pkg-authors '("Taichi Uemura"))
