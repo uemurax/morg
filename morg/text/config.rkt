51c6535bb0d9f8ceb888543148bb8676bcd6f9da
Taichi Uemura <t.uemura00@gmail.com>
2023-06-06 15:27:40 +0200

    document to text

--- morg/text/config.rkt
+++ morg/text/config.rkt
@@ -2,9 +2,16 @@
 
 (require "../data/node.rkt")
 
-(provide (struct-out config) Config)
+(provide (struct-out config) Config
+         (struct-out user-config) UserConfig)
 
 (struct config
-  ([node-table : NodeTable])
+  ([user-config : UserConfig]
+   [node-table : NodeTable])
   #:transparent
   #:type-name Config)
+
+(struct user-config
+  ()
+  #:transparent
+  #:type-name UserConfig)
---
+++ morg/text/document.rkt
@@ -0,0 +1,32 @@
+#lang at-exp typed/racket
+
+(require "../data/document.rkt"
+         "../markup/string.rkt"
+         "../data/node.rkt"
+         "../util/list.rkt"
+         "section.rkt"
+         "inline.rkt"
+         "config.rkt")
+
+(define ((document->text [user-cfg : UserConfig]) [doc : Document]) : StringTree
+  (define front (document-front doc))
+  (define main (document-main doc))
+  (define back (document-back doc))
+  (define cfg
+    (config user-cfg
+            (make-node-table main)))
+  (define f (inline->text cfg))
+  (define g (section->text cfg))
+  @string~{
+    @(f (document-title doc))
+    ========================================
+
+    @(apply string~
+            (list-join-1
+             (map f (document-author doc))
+             ", "))
+
+    @(apply string~ (map g front))
+    @(apply string~ (map g main))
+    @(apply string~ (map g back))
+  })
