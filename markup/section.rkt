3c3816fa473387b45bc5314e8050ec5eb023c69b
Taichi Uemura <t.uemura00@gmail.com>
2023-06-06 11:58:18 +0200

    section markup

---
+++ markup/section.rkt
@@ -0,0 +1,27 @@
+#lang typed/racket
+
+(require "../data/section.rkt"
+         "../data/id.rkt"
+         "../data/article.rkt"
+         "inline.rkt"
+         "block.rkt")
+
+(provide section~)
+
+(define-type SectionElementLike
+  (U Article BlockLike))
+
+(define (section-element-like->section-element [x : SectionElementLike]) : SectionElement
+  (cond
+   [(article? x) x]
+   [((make-predicate BlockLike) x)
+    (block~ x)]))
+
+(define (section~ #:id [maybe-id : String]
+                  #:title [title : InlineLike]
+                  #:contents [contents : (Listof SectionElementLike) (list)]
+                  . [subsections : Section *]) : Section
+  (section (id maybe-id)
+           (inline~ title)
+           (map section-element-like->section-element contents)
+           subsections))
