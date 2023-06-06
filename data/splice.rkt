b8ccae15d291b65718e3f3d4122350f51ec7eb9c
Taichi Uemura <t.uemura00@gmail.com>
2023-06-06 11:15:44 +0200

    fix splice

--- data/splice.rkt
+++ data/splice.rkt
@@ -1,8 +1,13 @@
 #lang typed/racket
 
-(provide (struct-out splice) Splice)
+(provide (struct-out splice) Splice
+         splice-map)
 
 (struct (X) splice
-  ([contents : X])
+  ([contents : (Listof X)])
   #:transparent
   #:type-name Splice)
+
+(define #:forall (X Y)
+        (splice-map [f : (X . -> . Y)] [a : (Splice X)]) : (Splice Y)
+  (splice (map f (splice-contents a))))
