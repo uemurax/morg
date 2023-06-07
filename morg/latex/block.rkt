ad4f2bd9d8bca45d6952b1dbce25504de9631127
Taichi Uemura <t.uemura00@gmail.com>
2023-06-07 23:57:05 +0200

    some implementation

--- morg/latex/block.rkt
+++ morg/latex/block.rkt
@@ -1,13 +1,24 @@
-#lang typed/racket
+#lang at-exp typed/racket
 
 (require "../data/block.rkt"
          (prefix-in tex: "../data/tex.rkt")
+         "../markup/tex.rkt"
+         "inline.rkt"
          "config.rkt")
 
 (provide block->latex)
 
 (: block->latex : (Config . -> . (Block . -> . tex:TextTeX)))
 
+(define ((paragraph->latex [cfg : Config])
+          [x : Paragraph]) : tex:TextTeX
+  @text-tex~{
+    
+    @((inline->latex cfg) (paragraph-contents x))
+  })
+
 (define ((block->latex cfg) b)
+  (define x (block-contents b))
   (cond
+   [(paragraph? b) ((paragraph->latex cfg) b)]
    [else (error "Unimplemented.")]))
--- morg/latex/inline.rkt
+++ morg/latex/inline.rkt
@@ -1,13 +1,25 @@
-#lang typed/racket
+#lang at-exp typed/racket
 
 (require "../data/inline.rkt"
          (prefix-in tex: "../data/tex.rkt")
+         "../markup/tex.rkt"
          "config.rkt")
 
 (provide inline->latex)
 
 (: inline->latex : (Config . -> . (Inline . -> . tex:TextTeX)))
 
+(define ((text->latex [_cfg : Config])
+          [x : Text]) : tex:TextTeX
+  @text-tex~{@(text-contents x)})
+
+(define ((math->latex [_cfg : Config])
+         [x : Math]) : tex:TextTeX
+  (tex:text-tex (tex:math (math-contents x))))
+
 (define ((inline->latex cfg) i)
+  (define x (inline-contents i))
   (cond
+   [(text? x) ((text->latex cfg) x)]
+   [(math? x) ((math->latex cfg) x)]
    [else (error "Unimplemented.")]))
