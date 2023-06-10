a2070f78bb34ca3652bc3029fb8d948c56b4d3d1
Taichi Uemura <t.uemura00@gmail.com>
2023-06-10 16:53:25 +0200

    update text output

--- morg/text/inline.rkt
+++ morg/text/inline.rkt
@@ -15,9 +15,6 @@
 (provide inline->text)
 
 (: inline->text : (Config . -> . (Inline . -> . StringTree)))
-(: text->text : (Config . -> . (Text . -> . StringTree)))
-(: ref->text : (Config . -> . (Ref . -> . StringTree)))
-(: math->text : (Config . -> . (Math . -> . StringTree)))
 
 (define ((inline->text cfg) i)
   (define x (inline-contents i))
@@ -26,12 +23,13 @@
    [(splice? x) ((splice->text (inline->text cfg)) x)]
    [(ref? x) ((ref->text cfg) x)]
    [(math? x) ((math->text cfg) x)]
+   [(unordered-list? x) ((unordered-list->text cfg) x)]
    [else (error "Unimplemented.")]))
 
-(define ((text->text _cfg) t)
+(define ((text->text [_cfg : Config]) [t : Text]) : StringTree
   (string% (text-contents t)))
 
-(define ((ref->text cfg) r)
+(define ((ref->text [cfg : Config]) [r : Ref]) : StringTree
   (define tbl (config-node-table cfg))
   (define id (ref-id r))
   (define in? (node-table-has-key? tbl id))
@@ -52,5 +50,15 @@
      [else @string%{}]))
   @string%{@|s|@(id->text id)})
 
-(define ((math->text _cfg) m)
+(define ((math->text [_cfg : Config]) [m : Math]) : StringTree
   @string%{\(@(math-tex->text (math-contents m))\)})
+
+(define ((list-item->text [cfg : Config])
+         [i : ListItem]) : StringTree
+  @string%{* @((inline->text cfg) (list-item-contents i))})
+
+(define ((unordered-list->text [cfg : Config])
+         [ul : UnorderedList]) : StringTree
+  @string%{
+    [@(apply % (map (list-item->text cfg) (unordered-list-contents ul)))]
+  })
