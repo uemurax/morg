7167a31fce78902e32fdf96540d6a54d6ce95a6e
Taichi Uemura <t.uemura00@gmail.com>
2023-06-10 14:59:15 +0200

    publish html

--- morg/html.rkt
+++ morg/html.rkt
@@ -3,8 +3,41 @@
 (require "html/site.rkt"
          "data/document.rkt")
 
-(provide ->html)
+(require/typed racket/file
+  [make-temporary-directory (-> Path)])
+
+(require/typed racket/base
+  [copy-file (Path Path Boolean . -> . Void)])
+
+(provide ->html
+         ->html/publish)
 
 (define (->html #:config [cfg : UserConfig default-config]
                 [doc : Document]) : Site
   (make-site cfg doc))
+
+(define ((write-page [dir : Path])
+         [url : String] [contents : String]) : Void
+  (define file (build-path dir url))
+  (call-with-output-file* file
+   (lambda ([port : Output-Port])
+     (write-string contents port)))
+  (void))
+
+(define (site-publish [dst-dir : Path] [s : Site]) : Void
+  (define tmp-dir (make-temporary-directory))
+  (hash-for-each s (write-page tmp-dir))
+  (for-each
+   (lambda ([f : Path])
+     (copy-file (build-path tmp-dir f)
+                (build-path dst-dir f)
+                #t)
+     (void))
+   (directory-list tmp-dir))
+  (void))
+
+(define (->html/publish #:config [cfg : UserConfig default-config]
+                        [doc : Document]
+                        [dst-dir : Path]) : Void
+  (define s (->html #:config cfg doc))
+  (site-publish dst-dir s))
