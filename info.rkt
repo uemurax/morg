6e7de83f9db867f43fa7ecc9589dcfe40801ff5b
Taichi Uemura <t.uemura00@gmail.com>
2023-06-11 23:13:03 +0200

    fix dependency

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 
 (define collection 'multi)
-(define deps '("base"))
+(define deps '("base" "typed-racket-lib"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define pkg-desc "Organize your mathematical thoughts.")
 (define version "0.0")
