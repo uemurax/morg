3881144a08b6bf4e3dbd374c4f2416daaae7403b
Taichi Uemura <t.uemura00@gmail.com>
2023-06-12 21:40:06 +0200

    update latex output

--- morg/latex/block.rkt
+++ morg/latex/block.rkt
@@ -4,6 +4,8 @@
          (prefix-in tex: "../data/tex.rkt")
          "../data/splice.rkt"
          "../markup/tex.rkt"
+         "../markup/index.rkt"
+         "../data/index-table.rkt"
          "inline.rkt"
          "splice.rkt"
          "config.rkt")
@@ -20,9 +22,26 @@
     @((inline->latex cfg) (paragraph-contents x))
   })
 
+(define ((print-index->latex [cfg : Config])
+         [p : PrintIndex]) : tex:TextTeX
+  (define usr-cfg (config-user-config cfg))
+  (define n (user-config-index-num-columns usr-cfg))
+  (define tbl (config-index-table cfg))
+  (define type (print-index-type p))
+  (define in? (index-table-has-key? tbl type))
+  (cond
+   [in?
+    @text-tex%{
+      @((inst environment% TextTeXLike) "multicols"
+        #:arguments (list @argument%[(number->string n)])
+        @((inline->latex cfg) (index-list->inline (index-table-ref tbl type))))
+    }]
+   [else @text-tex%{}]))
+
 (define ((block->latex cfg) b)
   (define x (block-contents b))
   (cond
    [(paragraph? x) ((paragraph->latex cfg) x)]
+   [(print-index? x) ((print-index->latex cfg) x)]
    [(splice? x) ((splice->latex (block->latex cfg)) x)]
    [else (error "Unimplemented.")]))
