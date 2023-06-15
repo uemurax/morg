43e675807a595e687c0a80dba75c2f78546fe328
Taichi Uemura <t.uemura00@gmail.com>
2023-06-15 16:28:04 +0200

    separate config and state

--- morg/text/config.rkt
+++ morg/text/config.rkt
@@ -5,24 +5,17 @@
          "../markup/string.rkt")
 
 (provide (struct-out config) Config
-         (struct-out user-config) UserConfig
          default-config)
 
+
 (struct config
-  ([user-config : UserConfig]
-   [index-table : IndexTable]
-   [node-table : NodeTable])
+  ([make-section-ref : (Natural String . -> . StringTree)])
   #:transparent
   #:type-name Config)
 
-(struct user-config
-  ([make-section-ref : (Natural StringTree . -> . StringTree)])
-  #:transparent
-  #:type-name UserConfig)
-
-(define (default-config:make-section-ref [_depth : Natural] [num : StringTree]) : StringTree
+(define (default-config:make-section-ref [_depth : Natural] [num : String]) : StringTree
   @string%{Section @|num|})
 
 (define default-config
-  (user-config
+  (config
    default-config:make-section-ref))
---
+++ morg/text/state.rkt
@@ -0,0 +1,12 @@
+#lang typed/racket
+
+(require "config.rkt"
+         "../data/index-table.rkt"
+         "../data/node.rkt")
+
+(struct state
+  ([config : Config]
+   [index-table : IndexTable]
+   [node-table : NodeTable])
+  #:transparent
+  #:type-name State)
