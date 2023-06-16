d7517293d5375c50ef7f1ffdf2af73e14e4c9103
Taichi Uemura <t.uemura00@gmail.com>
2023-06-16 12:38:47 +0200

    fix

--- morg/html/site.rkt
+++ morg/html/site.rkt
@@ -14,70 +14,26 @@
          "inline.rkt"
          "id.rkt"
          "document.rkt"
-         (submod "inline.rkt" style)
-         (submod "id.rkt" style)
-         (submod "toc.rkt" style)
-         (submod "article.rkt" style)
-         (submod "section.rkt" style)
-         (submod "block.rkt" style)
-         (submod "document.rkt" style)
-         "xexpr-table.rkt")
+         "config.rkt"
+         (submod "inline.rkt" style))
 
 (require/typed racket/hash
   [hash-union ((HashTable String String) (HashTable String String) . -> . (HashTable String String))])
 
-(provide (struct-out config) Config
-         (struct-out user-config) UserConfig
-         Assets
-         Site
-         make-site
-         default-config)
-
-(define-type Assets
-  (HashTable String StringTree))
-
-(struct config
-  ([user-config : UserConfig]
-   [root : Document])
-  #:transparent
-  #:type-name Config)
-
-(struct user-config
-  ([body-template : (Config (U Node Document) . -> . (XExprs . -> . XExprs))]
-   [head-template : (Config (U Node Document) . -> . (XExprs . -> . XExprs))]
-   [assets : Assets])
-  #:transparent
-  #:type-name UserConfig)
+(provide Site
+         make-site)
 
 (define-type Site
   (HashTable String String))
 
-(struct node-tables
-  ([front : NodeTable]
-   [main : NodeTable]
-   [back : NodeTable])
-  #:transparent
-  #:type-name NodeTables)
-
-(define (node-tables-ref [doc : Document] [tbls : NodeTables] [id : Id])
-        : (U Node Document)
-  (match tbls
-   [(node-tables front main back)
-    (cond
-     [(node-table-has-key? front id) (node-table-ref front id)]
-     [(node-table-has-key? main id) (node-table-ref main id)]
-     [(node-table-has-key? back id) (node-table-ref back id)]
-     [else doc])]))
-
-(define ((apply-template [cfg : Config] [tbls : NodeTables])
+(define ((apply-template [cfg : Config] [st : SiteState])
          [i : Id] [x : XExprs]) : (Values String String)
-  (define usr-cfg (config-user-config cfg))
-  (define n (node-tables-ref (config-root cfg) tbls i))
+  (define n (site-state-node-ref st i))
   (define head
-    (((user-config-head-template usr-cfg) cfg n) 
-     (head-init cfg n)))
+    (((config-head-template cfg) st n) 
+     (head-init st n)))
   (define body
-    (((user-config-body-template usr-cfg) cfg n)
+    (((config-body-template cfg) st n)
      (tagged% 'main '() x)))
   (define html
     (car 
@@ -91,35 +47,28 @@
              "<!DOCTYPE html>\n"
              (xml:xexpr->string html)))))
 
-(define (make-site [usr-cfg : UserConfig] [doc : Document]) : Site
+(define (make-site [cfg : Config] [doc : Document]) : Site
   (define pages
     (document->xexprs doc))
-  (define tbls
-    (node-tables
+  (define st
+    (site-state
      (make-node-table (document-front doc))
      (make-node-table (document-main doc))
-     (make-node-table (document-back doc))))
-  (define cfg
-    (config usr-cfg doc))
+     (make-node-table (document-back doc))
+     doc))
   (define site
-    (hash-map/copy pages (apply-template cfg tbls)))
+    (hash-map/copy pages (apply-template cfg st)))
   (define assets
-    (hash-map/copy (user-config-assets usr-cfg)
-      (lambda ([k : String] [x : StringTree])
-        (values k (string-tree->string x)))))
+    (hash-map/copy (config-assets cfg)
+      (lambda ([k : String] [x : StringTreeLike])
+        (values k (string-tree-like->string x)))))
   (hash-union site assets))
-
-(define default-config:css-name "default.css")
-
-(define ((default-config:body-template [_cfg : Config] [_n : (U Node Document)])
-         [x : XExprs]) : XExprs
-  x)
 
 (define (js-escape [x : String])
   (escape (hash "\\" "\\\\") x))
 
-(define (head-init [cfg : Config] [n : (U Node Document)]) : XExprs
-  (define doc (config-root cfg))
+(define (head-init [st : SiteState] [n : (U Node Document)]) : XExprs
+  (define doc (site-state-root st))
   (define doc-title (document-title doc))
   (define title
     (cond
@@ -168,39 +117,3 @@
         }
       });
     })))
-
-(define ((default-config:head-template [_cfg : Config] [_n : (U Node Document)])
-         [x : XExprs]) : XExprs
-  (xexprs%
-   x
-   (tagged% 'link
-            `((rel "stylesheet")
-              (href ,default-config:css-name)))))
-
-(define default-config:css
-  @string%{
-    body {
-      font-family: sans-serif;
-      font-size: 112.5%;
-    }
-    main {
-      max-width: 800px;
-      margin: 40px auto;
-    }
-    @|id-css|
-    @|inline-css|
-    @|block-css|
-    @|toc-css|
-    @|article-css|
-    @|section-css|
-    @|document-css|
-  })
-
-(define default-config:assets
-  (hash default-config:css-name default-config:css))
-
-(define default-config
-  (user-config
-   default-config:body-template
-   default-config:head-template
-   default-config:assets))
