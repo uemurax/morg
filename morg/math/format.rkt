3b4cc533886802e80bcf003b3877903af924d51e
Taichi Uemura <t.uemura00@gmail.com>
2023-06-17 13:51:57 +0200

    rename config

--- morg/math/format.rkt
+++ morg/math/format.rkt
@@ -5,38 +5,38 @@
          "../data/splice.rkt"
          "../markup/tex.rkt")
 
-(provide (struct-out user-config) UserConfig
-         (struct-out config) Config
+(provide (struct-out config) Config
+         (struct-out state) State
          default-config
          format-math-tex+)
 
-(struct user-config
-  ([levels : (Listof Symbol)])
-  #:transparent
-  #:type-name UserConfig)
-
 (struct config
-  ([user-config : UserConfig]
-   [level : Level])
+  ([levels : (Listof Symbol)])
   #:transparent
   #:type-name Config)
 
+(struct state
+  ([config : Config]
+   [level : Level])
+  #:transparent
+  #:type-name State)
+
 (define default-config
-  (user-config
+  (config
    '(+ * generic-bin generic-rel comma)))
 
-(: format-math-tex+ : (Config . -> . (MathTeX+ . -> . MathTeX)))
+(: format-math-tex+ : (State . -> . (MathTeX+ . -> . MathTeX)))
 
-(define ((format-paren [cfg : Config])
+(define ((format-paren [st : State])
          [p : (Paren MathTeX+)]) : MathTeXLike
-  (define usr-cfg (config-user-config cfg))
-  (define ss (user-config-levels usr-cfg))
-  (define cur-lv (config-level cfg))
+  (define cfg (state-config st))
+  (define ss (config-levels cfg))
+  (define cur-lv (state-level st))
   (define lv (paren-level p))
-  (define cfg-1
-    (struct-copy config cfg
+  (define st-1
+    (struct-copy state st
      [level lv]))
-  (define f (format-math-tex+ cfg-1))
+  (define f (format-math-tex+ st-1))
   (define contents (f (paren-contents p)))
   (define comp (lv . (level-compare ss) . cur-lv))
   (case comp
@@ -46,24 +46,24 @@
     (define r (f (paren-right p)))
     (math-tex% l contents r)]))
 
-(define ((format-atom [cfg : Config])
+(define ((format-atom [st : State])
          [a : (Atom MathTeX+)]) : (Atom MathTeX)
-  ((atom-map (format-math-tex+ cfg)) a))
+  ((atom-map (format-math-tex+ st)) a))
 
-(define ((format-sub-sup [cfg : Config])
+(define ((format-sub-sup [st : State])
          [s : (SubSup (Atom MathTeX+) MathTeX+)]) : (SubSup (Atom MathTeX) MathTeX)
-  (define cfg-1
-    (struct-copy config cfg
+  (define st-1
+    (struct-copy state st
      [level (level #f 0)]))
-  (define cfg-2
-    (struct-copy config cfg
+  (define st-2
+    (struct-copy state st
      [level (level #t 0)]))
-  ((sub-sup-map (format-atom cfg-1) (format-math-tex+ cfg-2)) s))
+  ((sub-sup-map (format-atom st-1) (format-math-tex+ st-2)) s))
 
-(define ((format-math-tex+ cfg) m)
+(define ((format-math-tex+ st) m)
   (define x (math-tex+-contents m))
   (cond
-   [(atom? x) (math-tex ((format-atom cfg) x))]
-   [(splice? x) (math-tex (splice-map (format-math-tex+ cfg) x))]
-   [(paren? x) (math-tex% ((format-paren cfg) x))]
-   [(sub-sup? x) (math-tex ((format-sub-sup cfg) x))]))
+   [(atom? x) (math-tex ((format-atom st) x))]
+   [(splice? x) (math-tex (splice-map (format-math-tex+ st) x))]
+   [(paren? x) (math-tex% ((format-paren st) x))]
+   [(sub-sup? x) (math-tex ((format-sub-sup st) x))]))
