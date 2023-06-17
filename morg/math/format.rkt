58b1c4cff0e8a91ece240d00a73e73c3d47654f5
Taichi Uemura <t.uemura00@gmail.com>
2023-06-17 13:40:43 +0200

    fix formatter

--- morg/math/format.rkt
+++ morg/math/format.rkt
@@ -28,7 +28,7 @@
 (: format-math-tex+ : (Config . -> . (MathTeX+ . -> . MathTeX)))
 
 (define ((format-paren [cfg : Config])
-         [p : (Paren MathTeX+)]) : (Group MathTeX)
+         [p : (Paren MathTeX+)]) : MathTeXLike
   (define usr-cfg (config-user-config cfg))
   (define ss (user-config-levels usr-cfg))
   (define cur-lv (config-level cfg))
@@ -37,36 +37,33 @@
     (struct-copy config cfg
      [level lv]))
   (define f (format-math-tex+ cfg-1))
-  (define contents (group (f (paren-contents p))))
+  (define contents (f (paren-contents p)))
   (define comp (lv . (level-compare ss) . cur-lv))
   (case comp
    [(<) contents]
    [else
     (define l (f (paren-left p)))
     (define r (f (paren-right p)))
-    (group (math-tex% l contents r))]))
+    (math-tex% l contents r)]))
 
-(define ((format-atom+ [cfg : Config])
-         [a : (Atom+ MathTeX+)]) : (Atom MathTeX)
-  (define x (atom+-contents a))
-  (define f (format-math-tex+ cfg))
-  (cond
-   [(atom? x) ((atom-map f) x)]
-   [(paren? x) (atom ((format-paren cfg) x))]))
+(define ((format-atom [cfg : Config])
+         [a : (Atom MathTeX+)]) : (Atom MathTeX)
+  ((atom-map (format-math-tex+ cfg)) a))
 
 (define ((format-sub-sup [cfg : Config])
-         [s : (SubSup (Atom+ MathTeX+) MathTeX+)]) : (SubSup (Atom MathTeX) MathTeX)
+         [s : (SubSup (Atom MathTeX+) MathTeX+)]) : (SubSup (Atom MathTeX) MathTeX)
   (define cfg-1
     (struct-copy config cfg
      [level (level #f 0)]))
   (define cfg-2
     (struct-copy config cfg
      [level (level #t 0)]))
-  ((sub-sup-map (format-atom+ cfg-1) (format-math-tex+ cfg-2)) s))
+  ((sub-sup-map (format-atom cfg-1) (format-math-tex+ cfg-2)) s))
 
 (define ((format-math-tex+ cfg) m)
   (define x (math-tex+-contents m))
   (cond
-   [(atom+? x) (math-tex ((format-atom+ cfg) x))]
+   [(atom? x) (math-tex ((format-atom cfg) x))]
    [(splice? x) (math-tex (splice-map (format-math-tex+ cfg) x))]
+   [(paren? x) (math-tex% ((format-paren cfg) x))]
    [(sub-sup? x) (math-tex ((format-sub-sup cfg) x))]))
