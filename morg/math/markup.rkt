8a368e1d081a3af02db3d0fe8054d3046b29e792
Taichi Uemura <t.uemura00@gmail.com>
2023-06-17 13:45:53 +0200

    fix markup

--- morg/math/markup.rkt
+++ morg/math/markup.rkt
@@ -6,10 +6,8 @@
          "../util/list.rkt"
          "../markup/tex.rkt")
 
-(provide Atom+Like
-         MathTeXAtom+Like
+(provide MathTeXAtom+Like
          MathTeX+Like
-         atom+-like->atom+
          math-tex+-like->math-tex+
          paren%
          paren%/curried
@@ -21,27 +19,19 @@
          apply-with-parens%
          math-tex+%)
 
-(define-type (Atom+Like X)
-  (U (AtomLike X)
-     (Paren X)))
-
 (define-type MathTeXAtom+Like
-  (Atom+Like MathTeX+Like))
+  (AtomLike MathTeX+Like))
 
 (define-type MathTeX+Like
   (U MathTeX+
      MathTeXAtom+Like
      (Splice MathTeX+Like)
+     (Paren MathTeX+Like)
      (SubSup MathTeXAtom+Like MathTeX+Like)))
 
-(define #:forall (X)
-        (atom+-like->atom+ [x : (Atom+Like X)]) : (Atom+ X)
-  (cond
-   [(paren? x) (atom+ x)]
-   [else (atom+ (atom-like->atom x))]))
-
-(define (math-tex-atom+-like->math-tex-atom+ [x : MathTeXAtom+Like]) : (Atom+ MathTeX+)
-  ((atom+-map math-tex+-like->math-tex+) (atom+-like->atom+ x)))
+(define (math-tex-atom+-like->math-tex-atom+ [x : MathTeXAtom+Like]) : (Atom MathTeX+)
+  ((atom-map math-tex+-like->math-tex+)
+   (atom-like->atom x)))
 
 (define (math-tex+-like->math-tex+ [x : MathTeX+Like]) : MathTeX+
   (cond
@@ -50,6 +40,8 @@
     (math-tex+ (math-tex-atom+-like->math-tex-atom+ x))]
    [(splice? x)
     (math-tex+ (splice-map math-tex+-like->math-tex+ x))]
+   [(paren? x)
+    (math-tex+ ((paren-map math-tex+-like->math-tex+) x))]
    [(sub-sup? x)
     (math-tex+
      ((sub-sup-map math-tex-atom+-like->math-tex-atom+
