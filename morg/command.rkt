f4fc4733cef3d3b7cf2aa501c187edf733f11024
Taichi Uemura <t.uemura00@gmail.com>
2023-07-10 19:34:54 +0200

    parse command line arguments

--- morg/command.rkt
+++ morg/command.rkt
@@ -1,4 +1,40 @@
 #lang typed/racket
 
 (module+ main
-  (displayln "Hello, MOrg!"))
+  (define html? : (Parameterof Boolean)
+    (make-parameter #f))
+  (define pdf? : (Parameterof Boolean)
+    (make-parameter #f))
+  (define html-config-file : (Parameterof (Option String))
+    (make-parameter #f))
+  (define pdf-config-file : (Parameterof (Option String))
+    (make-parameter #f))
+  (define args
+    (command-line
+     #:once-each
+     ["--html" "Build HTML version."
+               (html? #t)]
+     ["--html-config" file
+                      "Config file for HTML output. Implies --html."
+                      (html? #t)
+                      (html-config-file (cast file String))]
+     ["--pdf" "Build PDF version."
+              (pdf? #t)]
+     ["--pdf-config" file
+                     "Config file for PDF output. Implies --pdf."
+                     (pdf? #t)
+                     (pdf-config-file (cast file String))]
+     #:args (index-file dst-dir)
+     (list index-file dst-dir)))
+  (define index-file
+    (cast (list-ref args 0) String))
+  (define dst-dir
+    (cast (list-ref args 1) String))
+  (displayln (format "Build HTML?: ~a" (html?)))
+  (when (html?)
+    (displayln (format "HTML config file: ~a" (html-config-file))))
+  (displayln (format "Build PDF?: ~a" (pdf?)))
+  (when (pdf?)
+    (displayln (format "PDF config file: ~a" (pdf-config-file))))
+  (displayln (format "Index file: ~a" index-file))
+  (displayln (format "Destination directory: ~a" dst-dir)))
