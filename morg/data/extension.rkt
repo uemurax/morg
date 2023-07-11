207214849aa312414a3363dfd3db50ed6827a6dc
Taichi Uemura <t.uemura00@gmail.com>
2023-07-11 21:28:58 +0200

    Revert "make extension transparent"
    This reverts commit 0fbe7348b15be09740fd865e125590b2f3a51b8f.

--- morg/data/extension.rkt
+++ morg/data/extension.rkt
@@ -8,13 +8,11 @@
 
 (struct ext-class
   ()
-  #:transparent
   #:type-name ExtClass)
 
 (struct (X) extension
   ([class : ExtClass]
    [contents : X])
-  #:transparent
   #:type-name Extension)
 
 (struct (X) ext-hash
@@ -30,7 +28,7 @@
             def))
 
 (define #:forall (X) (empty-ext-hash) : (ExtHash X)
-  ((inst ext-hash X) (hasheq)))
+  ((inst ext-hash X) (hash)))
 
 (define #:forall (X)
         (ext-hash-set [h : (ExtHash X)]
