6db221337eb29c6df248e90298dd2712251af5ca
Taichi Uemura <t.uemura00@gmail.com>
2023-07-11 21:00:55 +0200

    move example directory

--- morg/info.rkt
+++ morg/info.rkt
@@ -1,6 +1,5 @@
 #lang info
 
 (define scribblings '(("scribblings/morg.scrbl" ())))
-(define compile-omit-paths '("example/"))
 (define raco-commands
   '(("morg" (submod morg/command main) "run MOrg" #f)))
