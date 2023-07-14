5108b0deeea7506367cde1a0f31d913270bfedf8
Taichi Uemura <t.uemura00@gmail.com>
2023-07-14 18:52:50 +0200

    update package info

--- info.rkt
+++ info.rkt
@@ -1,8 +1,8 @@
 #lang info
 
 (define collection 'multi)
-(define deps '("base" "typed-racket-lib" "typed-racket-more"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "at-exp-lib" "rackunit-typed"))
+(define deps '("base" "typed-racket-lib" "typed-racket-more" "at-exp-lib"))
+(define build-deps '("scribble-lib" "at-exp-lib" "rackunit-typed"))
 (define pkg-desc "Organize your mathematical thoughts.")
 (define version "0.0")
 (define pkg-authors '("Taichi Uemura"))
